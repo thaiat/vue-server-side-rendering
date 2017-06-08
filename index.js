fbaf9d787dd5b0d8ab333dd3e0a0b460fe4d2d20
Avi Haiat <thaiat@ipelia.fr>
2017-06-08 04:57:18 +0300

    feat(app): Add more requests

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const axios = require('axios');
 const mail = require('./lib/mail');
 
 const EMAIL_TO = ['thaiat@yoobic.com'];
-axios.all([axios.get('https://randomuser.me/api/?page=3&results=10&seed=abc'), axios.get('https://randomapi.com/api/el3rc6as?key=2D3S-2MX2-EHXY-DDF6')])
-    .then(axios.spread((users, food) => {
-        console.log(food);
-        return users;
-    }))
-    .then(() => {
-        throw new Error('ee');
-    })
-    .then(response => response.data.results)
-    .then(users => {
-        console.time('generation');
+axios.all([
+        axios.get('https://randomuser.me/api/?page=3&results=10&seed=abc'),
+        axios.get('https://randomapi.com/api/el3rc6as?key=2D3S-2MX2-EHXY-DDF6'),
+        axios.get('http://hipsterjesus.com/api/?html=true')
+    ])
+    .then(axios.spread((userResponse, foodResponse, loremResponse) => {
         const data = {
             message: 'Yoo!!!!',
             display: false,
             color: 'yellow',
-            users: users,
-            card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+            users: userResponse.data.results,
+            food: foodResponse.data.results[0].food,
+            card_text: loremResponse.data.text
         };
+        return data;
+    }))
+    .then(data => {
+        console.time('generation');
 
         var app = appFactory(data, 'index.html', [
             require('./templates/components/Hello')
